32535e7d0d0fc75ae040e34e2da9085e7a790096
M1llyz <005jamilly@gmail.com>
2025-01-11 19:12:15 -0300

    meu primeiro commit

---
+++ Projeto1DIO/index.js
@@ -0,0 +1,16 @@
+console.log("Digite o nome do seu jogador:")
+
+// declarando a variaável nickname usando o comando *let*
+let nickname = "M1lloca"
+
+// concatenando uma mensagem fixa + uma variável (concatenar == unir)
+console.log("Bem vindo(a), " + nickname + "!")
+console.log(nickname + " entrou no servidor")
+
+// declarando uma constante (diferente da variável, a const não muda)
+const notificacao = "Pokemon GO:"
+
+
+// saída de notificação
+console.log(notificacao + " tem um novo pokemon na região!")
+console.log(notificacao + " você foi derrotado por um líder")
